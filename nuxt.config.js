4ca5698b6abeb77b3453ba78db9db305d041480d
kazuhide109 <55946276+kazuhide109@users.noreply.github.com>
2020-02-29 19:52:30 +0900

    third

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,15 @@
+/* nuxt.config.js */
+// `DEPLOY_ENV` が `GH_PAGES` の場合のみ `router.base = '/<repository-name>/'` を追加する
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+router: {
+  base: 'web'
+}
+} : {}
+
+export default {
+  ...routerBase
+}
+
 
 export default {
   mode: 'universal',
@@ -49,7 +61,6 @@ export default {
     extend (config, ctx) {
     }
   }
-  router: {
- 　 base: '/web/'
-  }
+  ...routerBase
+
 }
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <logo />
       <h1 class="title">
-        web
+        KAZUHIDE
       </h1>
       <h2 class="subtitle">
         My classy Nuxt.js project
